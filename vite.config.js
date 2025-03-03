2d582a2f485f70ec625d6bbaba4ec98f4f5c5fc2
Hildebrando dos Santos Filho <83103911+hildebrandofilho@users.noreply.github.com>
2025-03-03 10:43:36 -0300

    fix: news corrections vercel and api

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,16 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://cdn-dev.preoday.com",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "/api"), // Mantém o /api no destino
-        configure: (proxy) => {
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader("Access-Control-Allow-Credentials", "true");
-            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-          });
-        },
+   plugins: [react()],
+   server: {
+      proxy: {
+         "/api": {
+            target: "https://cdn-dev.preoday.com",
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/api/, ""), // Remove "/api" do caminho
+         },
       },
-    },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+   },
 });
